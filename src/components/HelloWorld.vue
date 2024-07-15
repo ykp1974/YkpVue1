a301614d054045228317645bef2e9513d7318224
ykp1974 <yappaykp1974@gmail.com>
2024-07-15 14:48:38 +0900

    Test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <!-- ここに <span> タグを追加 -->
+      <span id="spn1">表示用のテキスト</span>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      xFor a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>{{ data.title }}</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="link in data.links" :key="link.text">
+          <a :href="link.href">{{ link.text }}</a>
+      </li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
@@ -27,7 +27,13 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-  </div>
+    <!-- ボタンを追加 -->
+    <button id="btn1" v-on:click="handleButtonClick" test="123">クリック1</button>
+    <button id="btn2" v-on:click="handleButtonClick" test="456">クリック2</button>
+    <template v-for="link in data.parVals" :key="link.text">
+      <button v-on:click="handleButtonClick" :test="link.text">{{ link.text }}がクリックされました！</button>
+    </template>
+</div>
 </template>
 
 <script>
@@ -35,6 +41,68 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      data: {
+        title: "Vue.js アプリケーションへようこそ！",
+        links: [
+          {
+            text: "コア ドキュメント",
+            href: "https://vuejs.org"
+          },
+          {
+            text: "フォーラム",
+            href: "https://forum.vuejs.org"
+          },
+          {
+            text: "Community Chat",
+            href: "https://chat.vuejs.org"
+          },
+          ],
+        parVals: [
+          {
+            text: "123パラメータ1",
+            value: "値1"
+          },
+          {
+            text: "456パラメータ2",
+            value: "値2"
+          },
+        ]
+      }
+    }
+  },  
+  mounted() {
+    // // JSONデータのフェッチ処理
+    // fetch('c://wk//ykptest.json')
+    //   .then(response => response.json())
+    //   .then(jsonData => {
+    //     this.data.parVals = jsonData;
+    //   })
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
+    // FileReader APIを使用してローカルファイルを読み込む
+    const file = new File(['[JSONデータ]'], 'ykptest.json');
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const jsonData = JSON.parse(event.target.result);
+      this.data.parVals = jsonData;
+    };
+    reader.onerror = (error) => {
+      console.error(error);
+    };
+    reader.readAsText(file);
+  },
+  methods: {
+    handleButtonClick(event) {
+      const buttonLabel = event.target.getAttribute('test');
+      const outputText = `${buttonLabel}がクリックされました！`;
+      console.log(outputText);
+      document.getElementById('spn1').textContent = outputText;
+    }
   }
 }
 </script>
